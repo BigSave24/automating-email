c5371f84f2e952d7f24887ada76c4381c4cc840d
Kedmond24 <Kedmond24@live.com>
2021-09-17 00:52:56 -0400

    ADD function to save file data to a dictionay

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+file_name = str(input())
+lookup = {}
 
-# File operations
-file = open("./data/exam.csv", "r")
 
-for line in file.readlines():
-    print(line)
+def studentScores(fileName):
+
+    # File operations
+    file = open("./data/" + file_name, "r")
+
+    for line in file.readlines():
+        keys = line.split(',')
+        lookup.update({keys[0]: {
+            'Email': keys[0],
+            'lastName': keys[1],
+            'firstName': keys[2],
+            'problem_1_score': keys[3],
+            'problem_1_comments': keys[4],
+            'problem_2_score': keys[5],
+            'problem_2_comments': keys[6],
+            'problem_3_score': keys[7],
+            'problem_3_comments': keys[8].rstrip()
+        }})
+
+    print(lookup)
